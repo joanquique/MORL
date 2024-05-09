6d2c4977f69c706bb12ecd0df222901552849076
Joan Alvarez <joanquique@gmail.com>
2024-05-09 08:30:26 -0600

    first commit

---
+++ js/modal.js
@@ -0,0 +1,22 @@
+// Obtener referencias a elementos del DOM
+var modal = document.getElementById("modal");
+var loginButton = document.getElementById("loginButton");
+var closeButton = document.getElementsByClassName("close")[0];
+var registerLink = document.getElementById("registerLink");
+
+// Mostrar el modal cuando se haga clic en el botón de iniciar sesión
+loginButton.onclick = function() {
+    modal.style.display = "block";
+}
+
+// Ocultar el modal cuando se haga clic en el botón de cerrar
+closeButton.onclick = function() {
+    modal.style.display = "none"; 
+}
+
+// Ocultar el modal cuando se haga clic fuera de él
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
